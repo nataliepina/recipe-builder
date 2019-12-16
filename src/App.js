0f2fa7e7952e2fd64451c227cde6c98882c58810
Natalie Pina <natalie.a.pina@gmail.com>
2019-12-15 19:40:05 -0600

    display recipes, working search, initial style

--- src/App.js
+++ src/App.js
@@ -1,41 +1,62 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-require('dotenv').config()
-
+import React, {useEffect, useState} from 'react'
+import Recipe from './Recipe'
+import './App.css'
 
 const App = () => {
-  const app_id = process.env.APP_ID
-  const app_key = process.env.APP_KEY
-
-
-  const [counter, setCounter] = useState(0);
+  const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('chicken')
 
   useEffect(() => {
     getRecipes()
-  }, [])
+  }, [query])
+
+  const ENDPOINT = `/search?q=${query}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`
 
   const getRecipes = async () => {
-    const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${app_key}`
-    )
+    const response = await fetch(ENDPOINT)
     const data = await response.json()
-    console.log(data)
+    setRecipes(data.hits)
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
+
+  const getSearch = e => {
+    // prevent search from updating on every key entered
+    e.preventDefault()
+    setQuery(search)
   }
 
   return(
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text" />
+      <form className="search-form" onSubmit={getSearch}>
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
         <button
           className="search-button"
           type="submit"
         >
           Search
         </button>
-        <h2 onClick={() => setCounter(counter + 1)}>{counter}</h2>
       </form>
+      <div className="recipes">
+      {recipes.map(recipe => (
+        <Recipe 
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
+        />
+        ))}
+      </div>
     </div>
   )
 }
 
-export default App;
+export default App
